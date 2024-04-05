be69c3aba9a1e13c87a685a882bc21a5142b96f2
theo-ahnlabio <theo@ahnlab.io>
2024-04-05 17:40:48 +0900

    remove x86_64 arch

---
+++ ABCWalletLiteKit.xcframework/ios-arm64-simulator/ABCWalletLiteKit.framework/Headers/ABCWalletLiteKit.h
@@ -0,0 +1,19 @@
+//
+//  ABCWalletLiteKit.h
+//  ABCWalletLiteKit
+//
+//  Created by theo-2022 on 3/13/24.
+//
+
+#import <Foundation/Foundation.h>
+#import <ABCWalletLiteKit/abcwallet_ios.h>
+
+//! Project version number for ABCWalletLiteKit.
+FOUNDATION_EXPORT double ABCWalletLiteKitVersionNumber;
+
+//! Project version string for ABCWalletLiteKit.
+FOUNDATION_EXPORT const unsigned char ABCWalletLiteKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ABCWalletLiteKit/PublicHeader.h>
+
+
